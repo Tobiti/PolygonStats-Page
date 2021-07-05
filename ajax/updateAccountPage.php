670daf9dff76296422b3d0fbb330ca1685491712
Tobias Adloff <tadloff@mail.upb.de>
2021-07-05 23:26:57 +0200

    Add update file

---
+++ ajax/updateAccountPage.php
@@ -0,0 +1,36 @@
+<?php
+	require('config.php');
+	
+	$sql = "UPDATE `Session` s SET EndTime=COALESCE((SELECT MAX(TIMESTAMP) FROM SessionLogEntry WHERE SessionId=s.Id), s.EndTime) WHERE EndTime=\"0001-01-01 00:00:00.000000\";";
+	$result = $mysqli->query($sql) or die("database error:". mysqli_error($mysqli));
+	
+	$sql = "UPDATE `Account` a SET TotalMinutes=COALESCE((SELECT SUM(TIMESTAMPDIFF(MINUTE, StartTime, EndTime)) FROM `Session` s WHERE s.AccountId=a.Id AND EndTime<>\"0001-01-01 00:00:00.000000\"), 1);";
+	$result = $mysqli->query($sql) or die("database error:". mysqli_error($mysqli));
+	
+	$sql = "UPDATE `Account` a SET CaughtPokemon=COALESCE((SELECT COUNT(CaughtSuccess=1) FROM SessionLogEntry WHERE SessionId IN (SELECT s.Id FROM `Session` s WHERE s.AccountId=a.Id)), 0);";
+	$result = $mysqli->query($sql) or die("database error:". mysqli_error($mysqli));
+	
+	$sql = "UPDATE `Account` a SET EscapedPokemon=COALESCE((SELECT COUNT(*) FROM SessionLogEntry WHERE LogEntryType=\"Pokemon\" AND CaughtSuccess=0 AND SessionId IN (SELECT s.Id FROM `Session` s WHERE s.AccountId=a.Id)), 0);";
+	$result = $mysqli->query($sql) or die("database error:". mysqli_error($mysqli));
+	
+	$sql = "UPDATE `Account` a SET Pokestops=COALESCE((SELECT COUNT(*) FROM SessionLogEntry WHERE LogEntryType=\"Fort\" AND SessionId IN (SELECT s.Id FROM `Session` s WHERE s.AccountId=a.Id)), 0);";
+	$result = $mysqli->query($sql) or die("database error:". mysqli_error($mysqli));
+	
+	$sql = "UPDATE `Account` a SET Raids=COALESCE((SELECT COUNT(*) FROM SessionLogEntry WHERE LogEntryType=\"Raid\" AND SessionId IN (SELECT s.Id FROM `Session` s WHERE s.AccountId=a.Id)), 0);";
+	$result = $mysqli->query($sql) or die("database error:". mysqli_error($mysqli));
+	
+	$sql = "UPDATE `Account` a SET Rockets=COALESCE((SELECT COUNT(*) FROM SessionLogEntry WHERE LogEntryType=\"Rocket\" AND SessionId IN (SELECT s.Id FROM `Session` s WHERE s.AccountId=a.Id)), 0);";
+	$result = $mysqli->query($sql) or die("database error:". mysqli_error($mysqli));
+	
+	$sql = "UPDATE `Account` a SET TotalGainedStardust=COALESCE((SELECT SUM(StardustReward) FROM SessionLogEntry WHERE SessionId IN (SELECT s.Id FROM `Session` s WHERE s.AccountId=a.Id)), 0);";
+	$result = $mysqli->query($sql) or die("database error:". mysqli_error($mysqli));
+	
+	$sql = "UPDATE `Account` a SET TotalGainedXp=COALESCE((SELECT SUM(XpReward) FROM SessionLogEntry WHERE SessionId IN (SELECT s.Id FROM `Session` s WHERE s.AccountId=a.Id)), 0);";
+	$result = $mysqli->query($sql) or die("database error:". mysqli_error($mysqli));
+	
+	$sql = "UPDATE `Account` a SET ShinyPokemon=COALESCE((SELECT SUM(Shiny) FROM SessionLogEntry WHERE SessionId IN (SELECT s.Id FROM `Session` s WHERE s.AccountId=a.Id)), 0);";
+	$result = $mysqli->query($sql) or die("database error:". mysqli_error($mysqli));
+	
+	$sql = "UPDATE `Account` a SET MaxIV=COALESCE((SELECT SUM(*) FROM SessionLogEntry WHERE Attack=15 AND Defense=15 AND Stamina=15 AND LogEntryType<>\"EvolvePokemon\" AND SessionId IN (SELECT s.Id FROM `Session` s WHERE s.AccountId=a.Id)), 0);";
+	$result = $mysqli->query($sql) or die("database error:". mysqli_error($mysqli));
+?>
